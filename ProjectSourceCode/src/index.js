592382b8fff8608a8ee67a22dd29178bbf484bae
Kaylee Dehncke <kaylee.dehncke@colorado.edu>
2024-11-13 14:16:28 -0700

    first part of index.js

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -11,6 +11,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcryptjs'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+app.use(express.static('public'));
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
